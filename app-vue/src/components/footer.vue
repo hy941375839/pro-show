6db70de6fffdcaf4e99bf465837f3da0ff788999
huangyue <hy@qq.com>
2019-08-26 20:34:07 +0800

    修改了login.vue文件

---
+++ app-vue/src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<mt-tabbar>
+			<mt-tab-item>
+				<img style="width:72px;" src="../assets/index.png">
+			</mt-tab-item>
+			<mt-tab-item>
+				<img style="width:72px;" src="../assets/cart.png">
+			</mt-tab-item>
+			<mt-tab-item>
+				<img style="width:72px;" src="../assets/profile.png">
+			</mt-tab-item>
+		</mt-tabbar>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ app-vue/src/components/login.vue
@@ -0,0 +1,67 @@
+<template>
+	<div>
+		<div class="top">
+			<!-- <img src=""/> -->
+		</div>
+		<div class="login">
+			<mt-field class="uname" label="账号" placeholder="请输入您的用户名" @blur.native="Utest()"></mt-field>
+			<mt-field class="upwd" label="密码" placeholder="请输入您的用户名" @blur.native="Ptest()"></mt-field>
+			<div class="select">
+				<span class="userType">用户类型</span> 
+				<select class="option">
+					<option value="0">用户</option>
+					<option value="1">商家</option>
+					<option value="2">管理员</option>
+				</select>
+			</div>
+				<mt-button class="loginBtn" type="default" @click.native="Login">登录</mt-button>
+				<mt-button class="regBtn" type="default" @click.native="reg">注册</mt-button>
+		</div>
+		<div classs="bottom"></div>
+	</div>
+</template>
+<script>
+  export default{
+    data:{
+
+    },
+    methods:{
+      Utest(){
+        
+      },
+      Ptest(){},
+      Login(){
+
+      },
+      reg(){
+        this.$router.push('/reg');
+      }
+    }
+  }
+</script>
+<style scoped>
+	.uname,.upwd,.select{
+		height:60px;
+	}
+	.userType{
+		font-size: 16px;
+		margin-left: -157px;
+		margin-right: 33px;
+	}
+	.select{
+		line-height:65px;
+	}
+	.option{
+		width:90px;
+		height:32px;
+		font-size: 18px;
+	}
+	.loginBtn,.regBtn{
+		width:120px;
+		height:40px;
+		margin-top: 35px;
+	}
+	.loginBtn{
+		margin-right:50px;
+	}
+</style>
