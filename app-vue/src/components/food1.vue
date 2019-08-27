efb221a6ce0073ba795efb1cd9a41f7243f5037c
huangyue <hy@qq.com>
2019-08-27 20:35:52 +0800

    修改了food1.vue文件

--- app-vue/src/components/food1.vue
+++ app-vue/src/components/food1.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    
+    <div class="top">
+      <img class="topLeft" src="../assets/left.png">
+    </div>
+    <img class="bigImg" src="../assets/food1.jpg">
   </div>
 </template>
+<style scoped>
+  .top{
+    width:100%;
+    height:35px;
+    display: flex;
+    justify-content: space-between;
+    background: #f5c443;
+  }
+  .topLeft{
+    width:20px;
+    height:18px;
+  }
+  .bigImg{
+    width:84%;
+    margin-left:-56px;
+    margin-top: 10px;
+  }
+</style>
--- app-vue/src/router.js
+++ app-vue/src/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from './components/login.vue'
 import footer from './components/footer.vue'
+import food1 from "./components/food1.vue";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path:'/login',component:login},
-    {path:'/foot',component:footer}
+    {path:'/foot',component:footer},
+    {path:'/food1',component:food1},
   ]
 })
